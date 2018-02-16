a312572478a5cf927a4541a81f522e3f238e5f02
Bhuwan Kandel <aanjanbhuwan@gmail.com>
2018-02-16 15:08:26 +0545

    simple complete calculator

---
+++ newApp/App.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react';
+import ReactCalculator from './src/components/ReactCalculator';
+
+
+export default class App extends Component{
+  render(){
+    return(
+      <ReactCalculator />
+    )
+  }
+}
---
+++ newApp/src/components/InputButton.js
@@ -0,0 +1,44 @@
+// InputButton.js
+
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableHighlight
+} from 'react-native';
+
+
+export default class InputButton extends Component {
+
+    render() {
+        return (
+            <TouchableHighlight style={[Style.inputButton, this.props.highlight ? Style.inputButtonHighlighted : null]}
+                                underlayColor="#193441"
+                                onPress={this.props.onPress}>
+                <Text style={Style.inputButtonText}>{this.props.value}</Text>
+            </TouchableHighlight>
+        )
+    }
+
+}
+
+var Style = StyleSheet.create({
+    inputButton: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 0.5,
+        borderColor: '#91AA9D'
+},
+
+    inputButtonText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+
+    inputButtonHighlighted: {
+        backgroundColor: '#193441'
+    },
+});
